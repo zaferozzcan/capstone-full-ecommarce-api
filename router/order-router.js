8d7284c76cc6f08d3b59fa0b339b25de213495d3
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-11 21:23:41 -0500

    can do post request

--- router/order-router.js
+++ router/order-router.js
@@ -7,4 +7,14 @@ orderRouter.get("/:user_id", (req, res) => {
   res.send("user id-->" + req.params.user_id);
 });
 
+orderRouter.post("/", (req, res) => {
+  Order.create(req.body, (err, data) => {
+    if (!err) {
+      return res.status(201).send("order has been created");
+    } else {
+      return err.status(400).json({ error: err.message });
+    }
+  });
+});
+
 module.exports = orderRouter;
